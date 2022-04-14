30d9bf294e1377e92ff4fcf74313287853b2020b
NaoyaUC <narc.0119.brrs@gmail.com>
2022-04-14 20:59:24 +0900

    StoryBookの読込先を修正
    .storybook\main.js   "stories"からデフォルトで生成されるものを除外する

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,18 @@
 module.exports = {
-  // "stories": [
-  //   "../src/**/*.stories.mdx",
-  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  // ],
-  stories: ['../src/components/**/*.stories.js'],
+  //読み込むstoryを指定
+  "stories": [
+    // "../src/**/*.stories.mdx",
+    // "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/components/**/*.stories.js'
+  ],
+  //アドオン
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app"
   ],
+  //vue angular Svelte
   framework: "@storybook/react",
   core: {
     "builder": "@storybook/builder-webpack5"
