c38f3809ba37125818da9e96859c55506d49c611
NaoyaUC <narc.0119.brrs@gmail.com>
2022-04-14 20:47:18 +0900

    StroyBookでのコンポーネント表示可能

---
+++ src/components/TaskList.stories.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import TaskList from "./TaskList";
+import * as TaskStories from "./Task.stories";
+
+export default {
+  component: TaskList,
+  title: "TaskList",
+  //wrapper 
+  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+};
+
+const Template = (args) => <TaskList {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  //初期値を格納
+  tasks: [
+    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
+    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
+    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
+    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
+    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
+    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
+  ],
+};
+
+export const WithPinnedTasks = Template.bind({});
+WithPinnedTasks.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Default story.
+  tasks: [
+    ...Default.args.tasks.slice(0, 5),
+    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
+  ],
+};
+
+// Loading中
+export const Loading = Template.bind({});
+Loading.args = {
+  tasks: [],
+  loading: true,
+};
+
+//空の場合
+export const Empty = Template.bind({});
+Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
+  ...Loading.args,
+  loading: false,
+};
